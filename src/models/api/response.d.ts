98d61e62ebd65d1dd60757c8d64008a9d597805a
voxuantu81 <voxuantu1235@gmail.com>
2024-01-09 09:46:20 +0700

    add message code to response

--- src/models/api/response.d.ts
+++ src/models/api/response.d.ts
@@ -1,6 +1,7 @@
 declare interface IResponse<T> {
-	status: string;
-	status_code: number;
-	message: string;
-	data: T;
+  status: string;
+  status_code: number;
+  message_code: string;
+  message: string;
+  data: T;
 }
